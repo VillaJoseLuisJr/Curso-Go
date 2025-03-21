82651d8dce3d2deb0fd1efdb5f7aa6bd575642b1
Junior <villa.joseluisjr@gmail.com>
2025-03-21 18:41:22 -0300

    Me quedé en Enums

--- goByExample/main.go
+++ goByExample/main.go
@@ -2,24 +2,56 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func fact(n int) int {
-	if n == 0 {
-		return 1
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func detectCircle(g geometry) {
+	if c, ok := g.(circle); ok {
+		fmt.Println("Circle with radius", c.radius)
 	}
-	return n * fact(n-1)
 }
 
 func main() {
-	fmt.Println(fact(7))
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
 
-	var fib func(n int) int
-	fib = func(n int) int {
-		if n < 2 {
-			return n
-		}
-		return fib(n-1) + fib(n-2)
-	}
-	fmt.Println(fib(7))
+	measure(r)
+	measure(c)
+
+	detectCircle(r)
+	detectCircle(c)
 }
