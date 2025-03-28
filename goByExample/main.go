5c5b64f1990c3318d213ae59af45559a73b85cc4
VillaJoseLuisJr <villa.joseluisjr@gmail.com>
2025-03-28 16:53:54 -0300

    Quedé en Writing Files

--- goByExample/main.go
+++ goByExample/main.go
@@ -1,83 +1,60 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
+	"io"
 	"os"
-	"strings"
 )
 
-type response1 struct {
-	Page   int
-	Fruits []string
-}
-
-type response2 struct {
-	Page   int      `json:"page"`
-	Fruits []string `json:"fruits"`
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-	bolB, _ := json.Marshal(true)
-	fmt.Println(string(bolB))
 
-	intB, _ := json.Marshal(1)
-	fmt.Println(string(intB))
+	dat, err := os.ReadFile("/tmp/dat")
+	check(err)
+	fmt.Print(string(dat))
 
-	fltB, _ := json.Marshal(2.34)
-	fmt.Println(string(fltB))
+	f, err := os.Open("/tmp/dat")
+	check(err)
 
-	strB, _ := json.Marshal("gopher")
-	fmt.Println(string(strB))
+	b1 := make([]byte, 5)
+	n1, err := f.Read(b1)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
 
-	slcD := []string{"apple", "peach", "pear"}
-	slcB, _ := json.Marshal(slcD)
-	fmt.Println(string(slcB))
+	o2, err := f.Seek(6, io.SeekStart)
+	check(err)
+	b2 := make([]byte, 2)
+	n2, err := f.Read(b2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: ", n2, o2)
+	fmt.Printf("%v\n", string(b2[:n2]))
 
-	mapD := map[string]int{"apple": 5, "lettuce": 7}
-	mapB, _ := json.Marshal(mapD)
-	fmt.Println(string(mapB))
+	_, err = f.Seek(2, io.SeekCurrent)
+	check(err)
 
-	res1D := &response1{
-		Page:   1,
-		Fruits: []string{"apple", "peach", "pear"}}
-	res1B, _ := json.Marshal(res1D)
-	fmt.Println(string(res1B))
+	_, err = f.Seek(-4, io.SeekEnd)
+	check(err)
 
-	res2D := &response2{
-		Page:   1,
-		Fruits: []string{"apple", "peach", "pear"}}
-	res2B, _ := json.Marshal(res2D)
-	fmt.Println(string(res2B))
+	o3, err := f.Seek(6, io.SeekStart)
+	check(err)
+	b3 := make([]byte, 2)
+	n3, err := io.ReadAtLeast(f, b3, 2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
 
-	byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
+	_, err = f.Seek(0, io.SeekStart)
+	check(err)
 
-	var dat map[string]interface{}
+	r4 := bufio.NewReader(f)
+	b4, err := r4.Peek(5)
+	check(err)
+	fmt.Printf("5 bytes: %s\n", string(b4))
 
-	if err := json.Unmarshal(byt, &dat); err != nil {
-		panic(err)
-	}
-	fmt.Println(dat)
-
-	num := dat["num"].(float64)
-	fmt.Println(num)
-
-	strs := dat["strs"].([]interface{})
-	str1 := strs[0].(string)
-	fmt.Println(str1)
-
-	str := `{"page": 1, "fruits": ["apple", "peach"]}`
-	res := response2{}
-	json.Unmarshal([]byte(str), &res)
-	fmt.Println(res)
-	fmt.Println(res.Fruits[0])
-
-	enc := json.NewEncoder(os.Stdout)
-	d := map[string]int{"apple": 5, "lettuce": 7}
-	enc.Encode(d)
-
-	dec := json.NewDecoder(strings.NewReader(str))
-	res1 := response2{}
-	dec.Decode(&res1)
-	fmt.Println(res1)
+	f.Close()
 }
